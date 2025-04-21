993ed823f286e10b35c43f487203002cea3f91b9
fortune onyeka <ihedfortune@gmail.com>
2025-04-21 13:06:23 -0300

    create user with user_params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,20 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to welcome_path(name: "#{@user.first_name} #{@user.last_name}"), 
+      notice: "Successfully added #{@user.first_name} #{@user.last_name} "
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name) # Add other permitted attributes
+  end
 end
